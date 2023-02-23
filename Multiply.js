39625080715781241ba265584407bd1418101dd4
Eihadyne <120111281+Eihadyne@users.noreply.github.com>
2023-02-23 10:16:31 +0000

    Create Multiply.js
    Multiply function that multiplies input by 2

---
+++ Multiply.js
@@ -0,0 +1,11 @@
+// Defined a value within a variable to be multiplied
+const value = 3;
+
+// Function takes in num parameter and multiplies the input by 2
+function multiply(num) {
+  const result = num * 2;
+  return result;
+}
+
+// Logs result of multiply function with value number
+console.log(multiply(value));
